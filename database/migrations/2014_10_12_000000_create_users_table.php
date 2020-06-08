23da3ff6e2c25797f6df99e34d825bbd347a9490
Archila <ivanestarchila@gmail.com>
2020-06-08 12:58:28 -0600

    Eliminado funcion unique en tabla users 2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
+            $table->string('name',100);
+            $table->string('email',100);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',20);
             $table->rememberToken();
             $table->timestamps();
         });
