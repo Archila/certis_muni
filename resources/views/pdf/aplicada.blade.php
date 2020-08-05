a013b1bc433802f18ef411b30a35b77add060e6b
Archila <ivanestarchila@gmail.com>
2020-08-05 15:31:07 -0600

    Prueba Orbis 2

--- resources/views/pdf/aplicada.blade.php
+++ resources/views/pdf/aplicada.blade.php
@@ -40,7 +40,7 @@
         .logo{
             height: 2cm;
             weight: 2cm;     
-            margin: 0.2em 0 0 0;
+            margin: 0.2em 0 -0.5em 0;
         }   
         .destinatario{
             display: block;
