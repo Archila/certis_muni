f186323496f4de4c01e8613e1da753711ae4cb12
Archila <ivanestarchila@gmail.com>
2022-02-01 21:56:53 -0600

    Agregado primer semestre de 2022 al select

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -23,6 +23,7 @@
       <a href="{{route('inicio.index', ['year'=>2020, 'semestre'=>2])}}" class="dropdown-item">Semestre 2 - 2020</a>
       <a href="{{route('inicio.index', ['year'=>2021, 'semestre'=>1])}}" class="dropdown-item">Semestre 1 - 2021</a>
       <a href="{{route('inicio.index', ['year'=>2021, 'semestre'=>2])}}" class="dropdown-item">Semestre 2 - 2021</a>
+      <a href="{{route('inicio.index', ['year'=>2022, 'semestre'=>1])}}" class="dropdown-item">Semestre 1 - 2022</a>
     </div>
   </li>
 </ul>
